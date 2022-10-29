cd821efec19926577591581432785096600765cd
dibkb <dibas9110@gmail.con>
2022-10-29 23:25:27 +0530

    Bottom header bar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,25 +4,37 @@ import { AmazonLogo } from "../../public/svg/Amazonlogo";
 import AccInfo from "./Header Components/AccInfo";
 // react icons
 import { BsSearch } from "react-icons/bs";
+import { HiMenu } from "react-icons/hi";
 import { Cart } from "./Header Components/Cart";
 const Header = () => {
   return (
-    <div className={styles["container"]}>
-      <div className={styles["logo"]}>
-        <AmazonLogo />
+    <>
+      <div className={styles["container"]}>
+        <div className={styles["logo"]}>
+          <AmazonLogo />
+        </div>
+        <div className={styles["search"]}>
+          <input type="text" placeholder="Search for anything" />
+          <button>
+            <BsSearch />
+          </button>
+        </div>
+        <div className={styles["account-info"]}>
+          <AccInfo heading={"Hello,John"} text={"Accounts & Lists"} />
+          <AccInfo heading={"Returns"} text={"& Orders"} />
+          <Cart item={2} />
+        </div>
       </div>
-      <div className={styles["search"]}>
-        <input type="text" placeholder="Search for anything" />
-        <button>
-          <BsSearch />
-        </button>
+      <div className={styles["header-bottom"]}>
+        <span>
+          <HiMenu />
+          <p>All</p>
+        </span>
+        <span>Prime Video</span>
+        <span>Amazon Business</span>
+        <span>Today&apos;s Deals</span>
       </div>
-      <div className={styles["account-info"]}>
-        <AccInfo heading={"Hello,John"} text={"Accounts & Lists"} />
-        <AccInfo heading={"Returns"} text={"& Orders"} />
-        <Cart item={2} />
-      </div>
-    </div>
+    </>
   );
 };
 
