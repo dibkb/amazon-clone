d3d64a61e7916f2c096ecb5c6e317e238ba231d9
dibkb <dibas9110@gmail.con>
2022-10-29 19:14:31 +0530

    categories list

---
+++ public/categories.js
@@ -0,0 +1,57 @@
+[
+  {
+    name: "Amazon Devices",
+    iconname: "BsSpeaker",
+    iconLibrary: "bs",
+  },
+  {
+    name: "Back to School",
+    iconname: "AiOutlineScissor",
+    iconLibrary: "ai",
+  },
+  {
+    name: "Computers",
+    iconname: "BsLaptop",
+    iconLibrary: "bs",
+  },
+  {
+    name: "Electronics",
+    iconname: "FiHeadphones",
+    iconLibrary: "fi",
+  },
+  {
+    name: "Kitchen",
+    iconname: "MdKitchen",
+    iconLibrary: "md",
+  },
+  {
+    name: "Toys & Games",
+    iconname: "MdOutlineToys",
+    iconLibrary: "md",
+  },
+  {
+    name: "Sports & Equipments",
+    iconname: "MdOutlineSportsFootball",
+    iconLibrary: "md",
+  },
+  {
+    name: "Books",
+    iconname: "TbBooks",
+    iconLibrary: "tb",
+  },
+  {
+    name: "Movies,Music & Video Games",
+    iconname: "SlMusicTone",
+    iconLibrary: "sl",
+  },
+  {
+    name: "Men's Fashion",
+    iconname: "BsSpeaker",
+    iconLibrary: "bs",
+  },
+  {
+    name: "Women's Fashion",
+    iconname: "BsSpeaker",
+    iconLibrary: "bs",
+  },
+];
