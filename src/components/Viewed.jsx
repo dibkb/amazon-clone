9f16824ca8f43b011b549c5b0675c23d31868967
dibkb <dibas9110@gmail.con>
2022-10-31 01:06:59 +0530

    History section

--- src/components/Viewed.jsx
+++ src/components/Viewed.jsx
@@ -1,5 +1,7 @@
 import React, { useEffect, useState } from "react";
+import Image from "next/image";
 import styles from "../../styles/Viewed.module.scss";
+import { toRupee } from "../../public/utils/functions";
 const Viewed = () => {
   const [viewed, setViewed] = useState();
   useEffect(() => {
@@ -15,12 +17,37 @@ const Viewed = () => {
     };
     fetchProducts();
   }, []);
+  console.log(viewed);
   return (
     <div className={styles["viewed-container"]}>
       <header>
         <h3>Your Recently Viewed Items</h3>
         <a href="#">View All</a>
       </header>
+      <main className={styles["items-container"]}>
+        {viewed?.map((element, id) => {
+          return (
+            <div key={element.title} className={styles["viewed-item"]}>
+              <span className={styles["image"]}>
+                <Image
+                  src={element.image}
+                  alt={element.title}
+                  height={50}
+                  width={50}
+                  object-fit={"contain"}
+                />
+              </span>
+              <div className={styles["info"]}>
+                <span>
+                  <p>{element.title}</p>
+                  <em>₹ {toRupee(element.price)}</em>
+                </span>
+                <button>Add to Cart</button>
+              </div>
+            </div>
+          );
+        })}
+      </main>
     </div>
   );
 };
